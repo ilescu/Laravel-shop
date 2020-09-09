715578f1b3c1179b631d3e642011239c47c3bc25
Ilescu_Anatolii <ilescu.anatolii@yahoo.com>
2020-09-09 18:05:34 +0200

    update
    update to commit 4

---
+++ app/Http/Middleware/BasketIsNotEmpty.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Order;
+use Closure;
+
+class BasketIsNotEmpty
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $orderId = session('orderId');
+        if (!is_null($orderId)) {
+            $order = Order::findOrFail($orderId);
+            if ($order->products->count() == 0) {
+                session()->flash('warning', 'Your basket is empty!');
+                return redirect()->route('index');
+            }
+        }
+        return $next($request);
+    }
+}
