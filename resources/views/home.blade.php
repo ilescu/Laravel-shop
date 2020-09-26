d279e54b91e13721a75841719615bbc815ac6a03
anatolii <ilescu.anatolii@yahoo.com>
2020-09-26 17:07:10 +0200

    demo comit
    pentru a vedea cum arata!

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,9 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
+asdas
                 <div class="card-body">
+                    <div class="card-body">pula mea</div>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
